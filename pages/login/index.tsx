3eb6a972b35b27ab2f7e03d6d60e6c29aeac590a
Bebika Singh <bebika@bebika.local>
2022-08-16 21:59:36 +0545

    change

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -98,7 +98,7 @@ const Login = () => {
 			</LoginBanner>
 			<LoginContent>
 				<LoginWrapper>
-					<h1 className="heading">Welcome Back To opps mistake !</h1>
+					<h1 className="heading">Welcome Back To opps mistakes !</h1>
 					<h3 className="sub-heading">Login to continue</h3>
 					<TextFieldWrapper>
 						<form>
