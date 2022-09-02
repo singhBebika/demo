1c937f32c3bfdce39e0af3d19c3d3c6e660c611b
singhBebika <05bebikasingh2016@gmail.com>
2022-09-02 09:45:45 +0545

    check build

---
+++ deploy_demo.sh
@@ -0,0 +1,30 @@
+
+#!/bin/bash
+cd ~/demo
+
+#export pm2 path
+export PATH=$PATH:/home/circleci/.nvm/versions/node/v16.15.0/bin
+#export yarn path
+export PATH=$PATH:/home/circleci/.nvm/versions/node/v16.15.0/bin
+
+yarn install
+
+if [ -d ".next" ]; then
+  rm -r .next/*
+else
+  mkdir .next
+fi;
+
+cp -r build/* .next/
+rm -rf build
+
+pm2 describe demo > /dev/null
+RUNNING=$?
+if [ "${RUNNING}" -ne 0 ]; then
+  pm2 start --name=demo yarn -- start
+  pm2 save --force
+else
+  pm2 restart demo
+  pm2 save --force
+fi;
+pm2 reload demo
