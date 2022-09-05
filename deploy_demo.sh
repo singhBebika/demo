c635e95b6e36650eee552b893f2af76f7609cc2a
singhBebika <05bebikasingh2016@gmail.com>
2022-09-06 00:09:04 +0545

    test

--- deploy_demo.sh
+++ deploy_demo.sh
@@ -3,11 +3,11 @@
 cd ~/demo
 
 #export pm2 path
-# export PATH=$PATH:/home/bebika/.npm-global/bin
+export PATH=$PATH:/home/bebika_singh/.nvm/versions/node/v16.15.0/bin
 #export yarn path
-export PATH=$PATH:/home/bebika/.nvm/versions/node/v16.10.0/bin
+export PATH=$PATH:/home/bebika_singh/.nvm/versions/node/v16.15.0/bin
 
-npm install
+yarn install
 
 if [ -d ".next" ]; then
   rm -r .next/*
@@ -18,3 +18,13 @@ fi;
 cp -r build/* .next/
 rm -rf build
 
+pm2 describe demo > /dev/null
+RUNNING=$?
+if [ "${RUNNING}" -ne 0 ]; then
+  pm2 start --name=demo yarn -- start
+  pm2 save --force
+else
+  pm2 restart demo
+  pm2 save --force
+fi;
+pm2 reload demo
