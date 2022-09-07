aeef22afa0e38e20ed86f41c2d0ce9600005fd3a
singhBebika <05bebikasingh2016@gmail.com>
2022-09-07 09:47:17 +0545

    transfer files

--- deploy_demo.sh
+++ deploy_demo.sh
@@ -21,7 +21,7 @@ rm -rf build
 pm2 describe demo > /dev/null
 RUNNING=$?
 if [ "${RUNNING}" -ne 0 ]; then
-  pm2 start --name=demo yarn -- start
+  pm2 start --name=demo npm -- start
   pm2 save --force
 else
   pm2 restart demo
