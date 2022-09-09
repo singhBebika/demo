5268937f1b56cf30f4ce4f46f9b14235c9ba2566
singhBebika <05bebikasingh2016@gmail.com>
2022-09-09 11:44:57 +0545

    test

--- deploy_demo.sh
+++ deploy_demo.sh
@@ -1,12 +1,17 @@
 
 #!/bin/bash
 cd ~/demo
-export PATH=$PATH:/home/bebika_singh/.nvm/versions/node/v16.10.0/bin
-export PATH=$PATH:/home/bebika_singh/.nvm/versions/node/v16.10.0/bin
-export PATH=~/.npm-global/bin:$PATH
-source ~/.profile
+export PATH=$PATH:/home/bebika_singh/.nvm/versions/node/v16.10.0/bebika_singh
+
 yarn install
 
 
-pm2 start "yarn start" --name demo
-pm2 save --force
+RUNNING=$?
+if [ "${RUNNING}" -ne 0 ]; then
+  pm2 start --name=demo yarn -- start
+  pm2 save --force
+else
+  pm2 restart demo
+  pm2 save --force
+fi;
+pm2 reload demo
