64a096bc07efb5a3da1bd7128166184119b4d591
singhBebika <05bebikasingh2016@gmail.com>
2022-09-09 11:37:30 +0545

    run script

--- deploy_demo.sh
+++ deploy_demo.sh
@@ -3,7 +3,8 @@
 cd ~/demo
 export PATH=$PATH:/home/bebika_singh/.nvm/versions/node/v16.10.0/bin
 export PATH=$PATH:/home/bebika_singh/.nvm/versions/node/v16.10.0/bin
-
+export PATH=~/.npm-global/bin:$PATH
+source ~/.profile
 yarn install
 
 
