94cbe2304f2e08806f34b5b0a2b0a97bf4a5a612
singhBebika <05bebikasingh2016@gmail.com>
2022-09-09 07:59:16 +0545

    permission issue

--- deploy_demo.sh
+++ deploy_demo.sh
@@ -6,15 +6,6 @@ npm install pm2 -g
 yarn install
 
 
-pm2 describe demo > /main/null
-RUNNING=$?
-if [ "${RUNNING}" -ne 0 ]; then
-  pm2 start --name=demo yarn bash -- start
-  pm2 save --force
-else
-  pm2 restart demo
-  pm2 save --force
-fi;
-pm2 reload demo
+pm2 start --name=demo yarn bash -- start
 pm2 list
 pm2 log
