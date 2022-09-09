450118fb34a88a98d71dc7d4a983450d4132ee6d
singhBebika <05bebikasingh2016@gmail.com>
2022-09-09 06:54:32 +0545

    permission issue

--- deploy_demo.sh
+++ deploy_demo.sh
@@ -17,7 +17,7 @@ rm -rf build
 pm2 describe demo > /dev/null
 RUNNING=$?
 if [ "${RUNNING}" -ne 0 ]; then
-  pm2 start --name=demo npm -- start
+  pm2 start --name=demo yarn -- start
   pm2 log
   pm2 save --force
 else
