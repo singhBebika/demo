10d4e3361da913a37f4d7c928648ef258a108668
singhBebika <05bebikasingh2016@gmail.com>
2022-09-09 06:53:10 +0545

    permission issue

--- deploy_demo.sh
+++ deploy_demo.sh
@@ -18,9 +18,11 @@ pm2 describe demo > /dev/null
 RUNNING=$?
 if [ "${RUNNING}" -ne 0 ]; then
   pm2 start --name=demo npm -- start
+  pm2 log
   pm2 save --force
 else
   pm2 restart demo
+  pm2 log
   pm2 save --force
 fi;
 pm2 reload demo
