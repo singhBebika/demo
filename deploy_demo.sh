263f30887b341a24281c8358a058fda32b73c81f
singhBebika <05bebikasingh2016@gmail.com>
2022-09-09 07:01:32 +0545

    permission issue

--- deploy_demo.sh
+++ deploy_demo.sh
@@ -11,18 +11,17 @@ else
   mkdir .next
 fi;
 
-cp -r build/* .next/
-rm -rf build
+cp -r .next/* .next/
+rm -rf .next
 
 pm2 describe demo > /dev/null
 RUNNING=$?
 if [ "${RUNNING}" -ne 0 ]; then
   pm2 start --name=demo yarn -- start
-  pm2 log
   pm2 save --force
 else
   pm2 restart demo
-  pm2 log
   pm2 save --force
 fi;
 pm2 reload demo
+pm2 log
